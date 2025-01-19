31719ad4b81b28bff4c1af103f9e6c858b6789d5
Armaan Chahal <armaan.c2009@gmail.com>
2025-01-19 01:43:39 -0600

    added zero page LDA and peek_byte()

--- src/main.cpp
+++ src/main.cpp
@@ -33,52 +33,67 @@ struct Mem
 };
 
 // 6502 microprocessor. 8 bit cpu, little endian
-struct CPU
-{
-  // the program counter
-  uint16_t PC;
-  // stack pointer
-  uint8_t SP;
+struct CPU {
+    // the program counter
+    uint16_t PC;
+    // stack pointer
+    uint8_t SP;
 
-  // registers
-  uint8_t A;
-  uint8_t X;
-  uint8_t Y;
+    // registers
+    uint8_t A;
+    uint8_t X;
+    uint8_t Y;
 
-  // flags
-  uint8_t C : 1;
-  uint8_t Z : 1;
-  uint8_t I : 1;
-  uint8_t D : 1;
-  uint8_t B : 1;
-  uint8_t O : 1;
-  uint8_t N : 1;
+    // flags
+    uint8_t C : 1;
+    uint8_t Z : 1;
+    uint8_t I : 1;
+    uint8_t D : 1;
+    uint8_t B : 1;
+    uint8_t O : 1;
+    uint8_t N : 1;
 
-  void reset(Mem& mem)
-  {
-    // reset the program counter
-    PC = 0xFFFC;
-    // reset the stack pointer
-    SP = 0x0100;
-    // reset the registers
-    A = X = Y = 0;
-    // reset the flags
-    C = Z = I = D = B = O = N = 0;
-    // initialize the memory. note that the CPU doesn't do anything else with it
-    mem.initialize();
-  }
+    void reset(Mem& mem)
+    {
+        // reset the program counter
+        PC = 0xFFFC;
+        // reset the stack pointer
+        SP = 0x0100;
+        // reset the registers
+        A = X = Y = 0;
+        // reset the flags
+        C = Z = I = D = B = O = N = 0;
+        // initialize the memory. note that the CPU doesn't do anything else with it
+        mem.initialize();
+    }
 
-  // takes a cycle
-  uint8_t fetch_byte(uint32_t& cycles, const Mem& memory)
-  {
-    uint8_t data = memory[PC];
-    PC++;
-    cycles--;
-    return data;
-  }
+    // fetches the byte of the PC. takes a cycle and increments program counter
+    uint8_t fetch_byte(uint32_t& cycles, const Mem& memory)
+    {
+        uint8_t data = memory[PC];
+        PC++;
+        cycles--;
+        return data;
+    }
+    // peeks a byte at an address. takes a cycle but does not increment program counter
+    uint8_t peek_byte(uint8_t address, uint32_t& cycles, const Mem& memory)
+    {
+        cycles--;
+        return memory[address];
+    }
+
 
   // opcodes
-  static constexpr uint8_t INS_LDA_IM = 0x00A9; // LDA immediate
+  static constexpr uint8_t
+    INS_LDA_IM = 0x00A9,    // LDA immediate
+    INS_LDA_ZP = 0x00A5,    // LDA Zero Page
+    INS_LDA_ZPX = 0x00B5;   // LDA Zero Page, X
+
+    void LDASetStatus()
+    {
+        Z = (A == 0);
+        N = (A & 0x80) != 0; // checks if the most significant digit of A is 1, for the 6502 it is checking for the 7th bit
+    }
 
   void execute(uint32_t cycles, Mem& memory)
   {
@@ -88,17 +103,26 @@ struct CPU
         switch (opCode)
         {
         case INS_LDA_IM:
-          {
+        {
             uint8_t val = fetch_byte(cycles, memory);
             A = val;
-            Z = (A == 0);
-            N = (A & 0x80) != 0; // checks if the most significant digit of A is 1, for the 6502 it is checking for the 7th bit
-          } break;
+            LDASetStatus();
+        } break;
+        case INS_LDA_ZP:
+        {
+            uint8_t ZeroPageAddress = fetch_byte(cycles, memory);
+            A = peek_byte(ZeroPageAddress, cycles, memory);
+            LDASetStatus();
+        } break;
+        case INS_LDA_ZPX:
+        {
+
+        } break;
 
         default:
-          {
-            std::cerr << "Unknown opcode: " << std::hex << (int)opCode << std::endl;
-          } break;
+        {
+            std::cerr << "Unknown opcode: " << "0x" << std::hex << (int)opCode << std::endl;
+        } break;
 
         }
       }
@@ -107,14 +131,15 @@ struct CPU
 
 int main()
 {
-  Mem mem;
-  CPU cpu;
-  cpu.reset(mem);
-  // inlining a little program
-  mem[0xFFFC] = CPU::INS_LDA_IM;
-  mem[0xFFFD] = 0x42;
-  // -------------------------
-  cpu.execute(2, mem);
-  std::cout << "A: " << std::hex << (int)cpu.A << std::endl;
-  return 0;
+    Mem mem;
+    CPU cpu;
+    cpu.reset(mem);
+    // inlining a little program
+    mem[0xFFFC] = CPU::INS_LDA_ZP;
+    mem[0xFFFD] = 0x42;
+    mem[0x0042] = 0x84;
+    // -------------------------
+    cpu.execute(3, mem);
+    std::cout << "A: " << "0x" << std::hex << (int)cpu.A << std::endl;
+    return 0;
 }
