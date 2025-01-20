9a57bac5cc00992cc19dceb113f132d28c743473
Armaan Chahal <armaan.c2009@gmail.com>
2025-01-20 14:30:40 -0600

    refactored switch statement in favor of function pointers.
    I decided to use grouped handlers instead of totally seperate lambdas. for each operator performance cost might outweigh the readability/maintenance benefit later down the line.

--- m6502Lib/src/6502.cpp
+++ m6502Lib/src/6502.cpp
@@ -3,105 +3,173 @@
 
 int32_t m6502::CPU::execute(int32_t cycles, Mem& memory)
 {
-        
     const int32_t cyclesRequested = cycles;
     while (cycles > 0)
     {
-        uint8_t opCode = fetch_byte(cycles, memory); // read opcode
-        switch (opCode)
-        {
-        case INS_LDA_IM:
-        {
-            A = fetch_byte(cycles, memory);
-            zn_set_status(A);
-        } break;
-        case INS_LDA_ZP:
-        {
-            uint8_t ZeroPageAddress = fetch_byte(cycles, memory);
-            A = peek_byte(ZeroPageAddress, cycles, memory);
-            zn_set_status(A);
-        } break;
-        case INS_LDA_ZPX:
-        {
-            uint8_t ZeroPageAddress = fetch_byte(cycles, memory);
-            ZeroPageAddress = wrap_zero_page(ZeroPageAddress + X);
-            cycles--; // adding X to the ZeroPageAddress takes a cycle
-            A = peek_byte(ZeroPageAddress, cycles, memory);
-            zn_set_status(A);
-        } break;
-        case INS_LDA_ABS:
-        {
-            uint16_t addr = fetch_word(cycles, memory);
-            A = peek_byte(addr, cycles, memory);
-            zn_set_status(A);
-        } break;
-        case INS_LDA_ABSX:
-        {
-            uint16_t baseAddr = fetch_word(cycles, memory);
-            uint16_t effectiveAddr = baseAddr + X;
-            if (crosses_page_boundary(effectiveAddr, baseAddr))
-            {
-                cycles--;
-            }
-            A = peek_byte(effectiveAddr, cycles, memory);
-            zn_set_status(A);
-        } break;
-        case INS_LDA_ABSY:
-        {
-            uint16_t baseAddr = fetch_word(cycles, memory);
-            uint16_t effectiveAddr = baseAddr + Y;
-            if (crosses_page_boundary(effectiveAddr, baseAddr))
-            {
-                cycles--;
-            }
-            A = peek_byte(effectiveAddr, cycles, memory);
-            zn_set_status(A);
-        } break;
-        case INS_LDA_INDX:
-        {
-            uint8_t zpAddr = fetch_byte(cycles, memory);
-            zpAddr = wrap_zero_page(zpAddr + X);
-            cycles--; // adding x to zpAddr takes a cycle
-            uint16_t effectiveAddr = peek_word(zpAddr, cycles, memory);
-            A = peek_byte(effectiveAddr, cycles, memory);
-            zn_set_status(A);
-        } break;
-        case INS_LDA_INDY:
-        {
-            uint8_t zpAddr = fetch_byte(cycles, memory);
-            uint16_t baseAddr = peek_word(zpAddr, cycles, memory);
-            uint16_t effectiveAddr = baseAddr + Y;
-            if (crosses_page_boundary(effectiveAddr, baseAddr))
-            {
-                cycles--;
-            }
-            A = peek_byte(effectiveAddr, cycles, memory);
-            zn_set_status(A);
-        } break;
-        case INS_JSR:
-        {
-            uint16_t SubAddr = fetch_word(cycles, memory);
-            // push return point - 1 on to the stack
-            memory.write_word(PC - 1, SP, cycles); // push return address on to the stack
-            SP = wrap_stack_address(SP - 1); // We decrement SP by 1 but also need to enforce 8-bit stack pointer wrapping
-            cycles--;
-            PC = SubAddr;
-            cycles--;
-        } break;
-
-        case 0xEA: // NOP opcode
-        {
-            cycles--; // It takes exactly 2 cycles
-        } break;
-
-        default:
-        {
-            std::cerr << "[ERROR] Unknown opcode: 0x" << std::hex << (int)opCode << std::endl;
-            return cyclesRequested - cycles; // Stop executing gracefully
-        } break;
-
-        }
+        uint8_t opCode = fetch_byte(cycles, memory);
+        auto handler = instructionTable[opCode];
+        handler(cycles, memory);
     }
 
     return cyclesRequested - cycles; // number of cycles used
+}
+
+void m6502::CPU::initialize_instruction_table()
+{
+    /*for (auto& entry : instructionTable)
+    {
+        entry = [&](int32_t& cycles, Mem& memory)
+        {
+            //std::cerr << "[ERROR] Unknown opcode." << std::endl;
+        };
+    }*/
+
+    // cycle accurate performance isn't a big concern of mine currently, hence I'm using shared handlers
+    // these shared handlers have a switch statement, but the few nanosecond runtime cost is a fine tradeoff for code maintenance
+    // maybe I'll change it later
+    
+    /** LDA family */
+    // ----------------------
+    instructionTable[INS_LDA_IM] = [&](int32_t& cycles, Mem& memory) {
+        handle_lda(cycles, memory, AddressingMode::Immediate);
+    };
+    instructionTable[INS_LDA_ZP] = [&](int32_t& cycles, Mem& memory) {
+        handle_lda(cycles, memory, AddressingMode::ZeroPage);
+    };
+    instructionTable[INS_LDA_ZPX] = [&](int32_t& cycles, Mem& memory) {
+        handle_lda(cycles, memory, AddressingMode::ZeroPageX);
+    };
+    instructionTable[INS_LDA_ABS] = [&](int32_t& cycles, Mem& memory) {
+        handle_lda(cycles, memory, AddressingMode::Absolute);
+    };
+    instructionTable[INS_LDA_ABSX] = [&](int32_t& cycles, Mem& memory) {
+        handle_lda(cycles, memory, AddressingMode::AbsoluteX);
+    };
+    instructionTable[INS_LDA_ABSY] = [&](int32_t& cycles, Mem& memory) {
+        handle_lda(cycles, memory, AddressingMode::AbsoluteY);
+    };
+    instructionTable[INS_LDA_INDX] = [&](int32_t& cycles, Mem& memory) {
+        handle_lda(cycles, memory, AddressingMode::IndirectX);
+    };
+    instructionTable[INS_LDA_INDY] = [&](int32_t& cycles, Mem& memory) {
+        handle_lda(cycles, memory, AddressingMode::IndirectY);
+    };
+    // ---------------------------------------------
+
+    /** LDX Family */
+    // ------------------
+    instructionTable[INS_LDX_IM] = [&](int32_t& cycles, Mem& memory)
+    {
+        handle_ldx(cycles, memory, AddressingMode::Immediate);
+    };
+    // -------------------
+
+    /** LDY Family */
+    // ----------------------
+    instructionTable[INS_LDY_IM] = [&](int32_t& cycles, Mem& memory)
+    {
+        handle_ldy(cycles, memory, AddressingMode::Immediate);
+    };
+    // ----------------------
+    
+    instructionTable[INS_JSR] = [&](int32_t& cycles, Mem& memory)
+    {
+        uint16_t SubAddr = fetch_word(cycles, memory);
+        // push return point - 1 on to the stack
+        memory.write_word(PC - 1, SP, cycles); // push return address on to the stack
+        SP = wrap_stack_address(SP - 1); // We decrement SP by 1 but also need to enforce 8-bit stack pointer wrapping
+        cycles--;
+        PC = SubAddr;
+        cycles--;
+    };
+    
+    // NOP opcode
+    instructionTable[0xEA] = [&](int32_t& cycles, Mem& memory) { cycles--; };
+}
+
+void m6502::CPU::handle_lda(int32_t& cycles, Mem& memory, AddressingMode mode)
+{
+    uint16_t address = 0;
+    switch (mode)
+    {
+    case AddressingMode::Immediate:
+        A = fetch_byte(cycles, memory);
+        zn_set_status(A);
+        return;
+
+    case AddressingMode::ZeroPage:
+        address = fetch_byte(cycles, memory);
+        break;
+
+    case AddressingMode::ZeroPageX:
+        address = wrap_zero_page(fetch_byte(cycles, memory) + X);
+        cycles--; // Extra cycle for indexed access
+        break;
+
+    case AddressingMode::Absolute:
+        address = fetch_word(cycles, memory);
+        break;
+
+    case AddressingMode::AbsoluteX:
+    {
+        uint16_t baseAddr = fetch_word(cycles, memory);
+        address = baseAddr + X;
+        if (crosses_page_boundary(address, baseAddr)) cycles--;
+        break;
+    }
+
+    case AddressingMode::AbsoluteY:
+    {
+        uint16_t baseAddr = fetch_word(cycles, memory);
+        address = baseAddr + Y;
+        if (crosses_page_boundary(address, baseAddr)) cycles--;
+        break;
+    }
+
+    case AddressingMode::IndirectX:
+    {
+        uint8_t zpAddr = wrap_zero_page(fetch_byte(cycles, memory) + X);
+        cycles--; // Extra cycle for zero-page indexing
+        address = peek_word(zpAddr, cycles, memory);
+        break;
+    }
+
+    case AddressingMode::IndirectY:
+    {
+        uint8_t zpAddr = fetch_byte(cycles, memory);
+        uint16_t baseAddr = peek_word(zpAddr, cycles, memory);
+        address = baseAddr + Y;
+        if (crosses_page_boundary(address, baseAddr)) cycles--;
+        break;
+    }
+    }
+    // Perform the LDA operation and update flags
+    A = peek_byte(address, cycles, memory);
+    zn_set_status(A);
+}
+
+void m6502::CPU::handle_ldx(int32_t& cycles, Mem& memory, AddressingMode addressingMode)
+{
+    uint16_t address = 0;
+    switch (addressingMode)
+    {
+    case AddressingMode::Immediate:
+    {
+        X = fetch_byte(cycles, memory);
+        zn_set_status(X);
+    } break;
+    }
+}
+
+void m6502::CPU::handle_ldy(int32_t& cycles, Mem& memory, AddressingMode addressingMode)
+{
+    uint16_t address = 0;
+    switch (addressingMode)
+    {
+    case AddressingMode::Immediate:
+    {
+        Y = fetch_byte(cycles, memory);
+        zn_set_status(Y);
+    } break;
+    }
 }
--- m6502Lib/src/6502.h
+++ m6502Lib/src/6502.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <cstdint>
 #include <memory>
+#include <functional>
 
 // modeling after the 6502 (see http://www.6502.org/users/obelisk/)
 // uint8_t = byte
@@ -12,7 +13,23 @@
 namespace m6502
 {
     struct Mem;
-    struct CPU;
+    class CPU;
+    
+    //using InstructionHandler = void (CPU::*)(int32_t& cycles, Mem& mem);
+    // Instruction Handler is a function pointer taking a ref to cycles and memory. 
+    using InstructionHandler = std::function<void(int32_t& cycles, Mem& memory)>;
+    
+    enum class AddressingMode : uint8_t {
+        Immediate,
+        ZeroPage,
+        ZeroPageX,
+        ZeroPageY,
+        Absolute,
+        AbsoluteX,
+        AbsoluteY,
+        IndirectX,
+        IndirectY
+    };
 }
 
 // 64 KB of memory
@@ -52,8 +69,9 @@ struct m6502::Mem
 };
 
 // 6502 microprocessor. 8-bit cpu, 16-bit memory bus, little endian
-struct m6502::CPU
+class m6502::CPU
 {
+public:
     // the program counter
     uint16_t PC;
     // stack pointer
@@ -74,16 +92,6 @@ struct m6502::CPU
     uint8_t V : 1;
     uint8_t N : 1;
 
-    inline uint16_t get_stack_address(uint8_t stackPointer)
-    {
-        return 0x0100 | stackPointer; // Stacks are always within page 0x01
-    }
-
-    inline uint8_t wrap_stack_address(uint8_t stackPointer)
-    {
-        return stackPointer & 0xFF; // enforce 8 bit stack pointer wrapping
-    }
-
     void reset(Mem& mem)
     {
         // reset the program counter
@@ -96,6 +104,64 @@ struct m6502::CPU
         C = Z = I = D = B = V = N = 0;
         // initialize the memory. note that the CPU doesn't do anything else with it
         mem.initialize();
+        
+        initialize_instruction_table();
+    }
+
+
+    // opcodes
+    static constexpr uint8_t
+        //LDA
+        INS_LDA_IM      = 0x00A9,   // LDA immediate
+        INS_LDA_ZP      = 0x00A5,   // LDA Zero Page
+        INS_LDA_ZPX     = 0x00B5,   // LDA Zero Page, X
+        INS_LDA_ABS     = 0x00AD,   // LDA Absolute
+        INS_LDA_ABSX    = 0x00BD,   // LDA Absolute, X
+        INS_LDA_ABSY    = 0x00B9,   // LDA Absolute, Y
+        INS_LDA_INDX    = 0x00A1,   // LDA Indirect, X
+        INS_LDA_INDY    = 0x00B1,   // LDA Indirect, X
+        // LDX
+        INS_LDX_IM      = 0x00A2,   // LDX Immediate
+        INS_LDX_ZP      = 0x00A6,   // LDX Zero Page
+        INS_LDX_ZPY     = 0x00B6,   // LDX Zero Page, Y
+        INS_LDX_ABS     = 0x00AE,   // LDX Absolute
+        INS_LDX_ABSY    = 0x00BE,   // LDX Absolute, Y
+    
+        // LDY
+        INS_LDY_IM      = 0x00A0,   // LDY Immediate
+        INS_LDY_ZP      = 0x00A4,   // LDX Zero Page
+        INS_LDY_ZPX     = 0x00B4,   // LDY Zero Page, X
+        INS_LDY_ABS     = 0x00AC,   // LDY Absolute
+        INS_LDY_ABSX    = 0x00BC,   // LDY Absolute, X
+    
+        INS_JSR         = 0x0020    // JSR Absolute
+    ;
+    
+    /** @return the number of cycles it took*/
+    int32_t execute(int32_t cycles, Mem& memory);
+
+private:
+
+    // 6502 has 256 total opcodes
+    InstructionHandler instructionTable[256];
+
+    void initialize_instruction_table();
+
+    // these grouped handlers have a switch statement based on addressingMoed increasing runtime cost. be aware of it if exact nanosecond cycle emulation is desired
+    void handle_lda(int32_t& cycles, Mem& memory, AddressingMode addressingMode);
+    void handle_ldx(int32_t& cycles, Mem& memory, AddressingMode addressingMode);
+    void handle_ldy(int32_t& cycles, Mem& memory, AddressingMode addressingMode);
+
+protected:
+    
+    inline uint16_t get_stack_address(uint8_t stackPointer)
+    {
+        return 0x0100 | stackPointer; // Stacks are always within page 0x01
+    }
+
+    inline uint8_t wrap_stack_address(uint8_t stackPointer)
+    {
+        return stackPointer & 0xFF; // enforce 8 bit stack pointer wrapping
     }
 
     // fetches the byte of the PC. takes a cycle and increments program counter
@@ -131,33 +197,7 @@ struct m6502::CPU
     }
 
 
-    // opcodes
-    static constexpr uint8_t
-        //LDA
-        INS_LDA_IM      = 0x00A9,   // LDA immediate
-        INS_LDA_ZP      = 0x00A5,   // LDA Zero Page
-        INS_LDA_ZPX     = 0x00B5,   // LDA Zero Page, X
-        INS_LDA_ABS     = 0x00AD,   // LDA Absolute
-        INS_LDA_ABSX    = 0x00BD,   // LDA Absolute, X
-        INS_LDA_ABSY    = 0x00B9,   // LDA Absolute, Y
-        INS_LDA_INDX    = 0x00A1,   // LDA Indirect, X
-        INS_LDA_INDY    = 0x00B1,   // LDA Indirect, X
-        // LDX
-        INS_LDX_IM      = 0x00A2,   // LDX Immediate
-        INS_LDX_ZP      = 0x00A6,   // LDX Zero Page
-        INS_LDX_ZPY     = 0x00B6,   // LDX Zero Page, Y
-        INS_LDX_ABS     = 0x00AE,   // LDX Absolute
-        INS_LDX_ABSY    = 0x00BE,   // LDX Absolute, Y
-    
-        // LDY
-        INS_LDY_IM      = 0x00A0,   // LDY Immediate
-        INS_LDY_ZP      = 0x00A4,   // LDX Zero Page
-        INS_LDY_ZPX     = 0x00B4,   // LDY Zero Page, X
-        INS_LDY_ABS     = 0x00AC,   // LDY Absolute
-        INS_LDY_ABSX    = 0x00BC,   // LDY Absolute, X
-    
-        INS_JSR         = 0x0020    // JSR Absolute
-    ;
+    #pragma region helpers
 
     // previously LDASetStatus(), but we can specify a register we want to pass in for this one.
     inline void zn_set_status(uint8_t value)
@@ -176,7 +216,6 @@ struct m6502::CPU
     {
         return address & 0x00FF; // Enforces zero-page boundaries
     }
-    
-    /** @return the number of cycles it took*/
-    int32_t execute(int32_t cycles, Mem& memory);
+
+    #pragma endregion
 };
